236ccb225193187bdcb3432868db4c3bf83ea22d
Aidan Curley <aidanculrey1012@gmail.com>
2020-10-07 20:54:56 -0400

    fix sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"aidancurley1012@gmail.com\"\n\
+echo "[general]
+email = \"aidancurley1012@gmail.com\"
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
