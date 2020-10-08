b954f4fc70c6417624473c1ff1481c5cbf4dbe2e
Aidan Curley <aidanculrey1012@gmail.com>
2020-10-07 20:38:06 -0400

    Update sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \aidancurley1012@gmail.com\”\n\
+email = \aidancurley1012@gmail.com\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
